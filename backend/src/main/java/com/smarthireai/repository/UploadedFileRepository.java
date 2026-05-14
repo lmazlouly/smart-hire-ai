29065c939d6075f92b117e8355c25515425d0e40
idou223 <halidoumaiga257@gmail.com>
2026-05-14 23:43:47 +0100

    Revert "R2 config"
    This reverts commit 0c6bb1661a719bf910f1e35c9d30e585d14294cd.

--- backend/src/main/java/com/smarthireai/repository/UploadedFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smarthireai.repository;
-
-import com.smarthireai.entity.UploadedFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadedFileRepository extends JpaRepository<UploadedFile, Long> {
-}
